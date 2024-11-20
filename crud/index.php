2d67bfcf92412542c27019e896bbff0fa5184ac8
Alexia Aubone <auboiguazu2024@gmail.com>
2024-11-20 17:13:21 -0300

    lenguajes

--- crud/index.php
+++ crud/index.php
@@ -102,7 +102,7 @@ if (!isset($_SESSION['nombre_usuario'])) {
             <a href="categ_read.php" class="btn btn-lg w-75 py-4" style="background-color: #a0522d; color: white;">CATEGORIAS</a>
           </div>
           <div class="col-md-6 mb-3">
-            <a href="consulta.php" class="btn btn-lg w-75 py-4" style="background-color: #8b4513; color: white;">CONSULTAS</a>
+            <a href="cons_read.php" class="btn btn-lg w-75 py-4" style="background-color: #8b4513; color: white;">CONSULTAS</a>
           </div>
         </div>
       </div>
